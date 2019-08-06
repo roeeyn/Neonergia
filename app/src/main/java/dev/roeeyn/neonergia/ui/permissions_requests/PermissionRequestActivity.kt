74e71b8731512140e9688fa3cad26968d64fc37c
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-08-06 14:03:49 -0300

    Finished permission activity layout

--- app/src/main/java/dev/roeeyn/neonergia/ui/permissions_requests/PermissionRequestActivity.kt
+++ app/src/main/java/dev/roeeyn/neonergia/ui/permissions_requests/PermissionRequestActivity.kt
@@ -12,7 +12,6 @@ class PermissionRequestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission_request)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
